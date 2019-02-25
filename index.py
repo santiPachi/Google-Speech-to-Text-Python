561f85e8ff67b3f49dbb4359a4fa30249c4e3cf6
Santiago Pacheco <santi@MacBook-Air-de-Santiago.local>
2019-02-25 09:39:42 -0500

    Agregado While

--- index.py
+++ index.py
@@ -3,11 +3,16 @@ import pyaudio
 
 
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Speak Anything :")
-    audio = r.listen(source)
-    try:
-        text = r.recognize_google(audio,language = 'es-ES')
-        print("You said : {}".format(text))
-    except:
-        print("Sorry could not recognize what you said")
+val = 'y'
+
+while val == 'y':
+    with sr.Microphone() as source:
+        print("Speak Anything :")
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio,language = 'es-ES')
+            print("You said : {}".format(text))
+        except:
+            print("Sorry could not recognize what you said")
+        
+        val = input('Desea volver a intentar y/n : ')
